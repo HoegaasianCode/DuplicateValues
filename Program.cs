9a675c981b0b8c61001786bbd8b5f0841274d75f
JoakimHoegaas <80755360+HoegaasianCode@users.noreply.github.com>
2023-09-28 12:08:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿List<string> strList = new();
+foreach (string line in File.ReadAllLines(
+@"C:\Users\Joakim.Hogas\Desktop\tekstMappe\parcelList1.txt"))
+{
+     strList.Add(line);
+}
+ IsDuplicateValue(strList);
+void IsDuplicateValue(List<string> strList)
+{
+    List<string> duplicateList = new();
+    foreach (string str in strList)
+    {
+        int i = 0;
+        foreach (string str1 in strList)
+        {
+            if (str1 == str || str1 == '0' + str || str1 == str.Remove(0, 1))
+            {
+                i++;
+            }
+        }
+        if (i > 1)
+        {
+            duplicateList.Add(str);
+            continue;
+        }
+    }
+    PrintDuplicateValues(duplicateList);
+}
+void PrintDuplicateValues(List<string> strList)
+{
+    foreach (string str in strList.Distinct())
+    {
+        Console.WriteLine(str);
+    }
+}
